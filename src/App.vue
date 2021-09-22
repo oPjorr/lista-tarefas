45ea51085a05d65ba46d168ab2c44026282f49b9
oPjorr <paulorolinski.ifc!@gmail.com>
2021-09-22 18:49:11 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template> 
+  <section>
+    <cabecalho :valor ="titulo"/>
+    <div class="container">
+      <div class="input-group">
+        <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text">
+        <span class="input-group-btn">
+          <button @click="adicionarTarefa" class="btn btn-primary">Adicionar tarefa</button>
+        </span>
+      </div>
+      <ul>
+        <li v-for="(tarefa, index) in tarefas" :key="index" :class="{ 'removed': tarefa.checked }">
+          <input type="checkbox" v-model="tarefa.checked">
+          <label> {{ tarefa.titulo }} </label>
+        </li>  
+      </ul>
+      <footer>
+        <em>Altere aqui o título da sua lista de tarefas</em>
+        <input v-model="titulo" type="text" />
+      </footer>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import cabecalho from '@/components/cabecalho'; 
+  export default {
+    name: 'App',
+    components: { cabecalho },
+    data() {
+      return {
+        titulo: "Minha lista de tarefas", 
+        novaTarefa: '',
+        tarefas: [
+          { titulo: "Estudar", checked: false },
+          { titulo: "Programar", checked: true },
+        ]  
+      };
+    },
+    methods: {
+      adicionarTarefa() {
+        this.tarefas.push({
+          titulo: this.novaTarefa,
+          checked: false,
+        });
+        this.novaTarefa = "";
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 40%;
+  padding-top: 50px;
+  margin: 20px auto 0 auto;
+}
+
+ul li {
+  list-style: none;
+}
+
+.removed {
+  color: red;
+}
+
+.removed label {
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/cabecalho.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <h1> {{valor}} </h1>
+  </header>
+</template>
+
+<script>
+export default {
+   props: ["valor"]
+};
+</script>
+
+<style>
+header {
+    top: 0;
+    display: flex;
+    margin-left: 0px;
+    height: 50px;
+    width: 100%;
+    color: #fff;
+    position: fixed;
+    align-items: center;
+    justify-content: center;
+    background: radial-gradient(circle, #0000FF 0, #191970 100%);
+}
+</style>
